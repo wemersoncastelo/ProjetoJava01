31a6f8edb902a47ced069dcee9aa703a9fac84f3
Wemerson_2 <Wemerson_2@192.168.1.101>
2022-11-22 17:54:15 -0400

    Criação da classe Pessoa

---
+++ ProjetoJava01/src/curso/java/Pessoa.java
@@ -0,0 +1,21 @@
+package curso.java;
+
+public class Pessoa {
+	private String nome;
+	private String sobrenome;
+	
+//	Getter and Setter
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+}
