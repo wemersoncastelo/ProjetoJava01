c83db2c228c374fab7d9f1906991a852284403fd
Wemerson_2 <Wemerson_2@192.168.1.101>
2022-11-28 18:47:58 -0400

    Aulas 27/11/2022

---
+++ ProjetoJava01/src/Modulo08/Aula05.java
@@ -0,0 +1,18 @@
+package Modulo08;
+
+public class Aula05 {
+
+	public static void main(String[] args) {
+		
+		int nota1 = 90;
+		int nota2 = 91;
+		
+		if (nota1 != nota2) {
+			System.out.println("As notas são diferentes!");
+		}else {
+			System.out.println("As notas são iguais!");
+		}
+		
+	}
+	
+}
---
+++ ProjetoJava01/src/Modulo08/Aula06.java
@@ -0,0 +1,21 @@
+package Modulo08;
+
+public class Aula06 {
+
+	public static void main(String[] args) {
+		
+		int num = 0;
+		/*
+		while(num <= 10) {
+			System.out.println("O número atual é: " + num + ".");
+			num ++;
+		}
+		*/
+		do {
+			System.out.println("O número atual é: " + num + ".");
+			num ++;
+		}while(num <= 10);
+		
+	}
+	
+}
