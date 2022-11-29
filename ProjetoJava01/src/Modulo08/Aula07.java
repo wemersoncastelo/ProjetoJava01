4a4a8ee21578a4ffe835c4f768f3332d189c1308
Wemerson_2 <Wemerson_2@192.168.1.101>
2022-11-28 20:39:21 -0400

    Aula 28/11/2022

---
+++ ProjetoJava01/src/Modulo08/Aula07.java
@@ -0,0 +1,13 @@
+package Modulo08;
+
+public class Aula07 {
+
+	public static void main(String[] args) {
+		
+		for(int num = 0; num <= 10; num++) {
+			System.out.println(num);
+		}
+		
+	}
+	
+}
---
+++ ProjetoJava01/src/Modulo08/Aula08.java
@@ -0,0 +1,21 @@
+package Modulo08;
+
+public class Aula08 {
+
+	public static void main(String[] args) {
+		
+		for(int num = 0; num <= 10; num ++) {
+			
+			if(num == 7) {
+				
+				System.out.println("Oba, encontrei o número 7!!!");
+				System.out.println("Fim!");
+				break;
+				
+			}
+			
+		}
+		
+	}
+	
+}
